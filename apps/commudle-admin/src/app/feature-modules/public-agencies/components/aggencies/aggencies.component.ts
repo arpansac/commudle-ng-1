eb8e9c6d0e293f3daeb5dcc041761cd5f5bbdf06
Arshdeep Singh <arsh199820@gmail.com>
2024-10-25 14:21:21 +0530

    fix: agencies page faqs

--- apps/commudle-admin/src/app/feature-modules/public-agencies/components/aggencies/aggencies.component.ts
+++ apps/commudle-admin/src/app/feature-modules/public-agencies/components/aggencies/aggencies.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { SeoService } from 'apps/shared-services/seo.service';
 import { FooterService } from 'apps/commudle-admin/src/app/services/footer.service';
 import { staticAssets } from 'apps/commudle-admin/src/assets/static-assets';
 import { CmsService } from 'apps/shared-services/cms.service';
 import { ITestimonial } from 'apps/shared-models/testimonial.model';
 import { IPreferredPartners } from 'apps/shared-models/preferred-partners.model';
-
+import { IFaq } from '@commudle/shared-models';
 @Component({
   selector: 'commudle-aggencies',
   templateUrl: './aggencies.component.html',
   styleUrls: ['./aggencies.component.scss'],
 })
-export class AggenciesComponent implements OnInit {
+export class AggenciesComponent implements OnInit, OnDestroy {
   staticAssets = staticAssets;
   stats: any[] = [
     {
@@ -33,6 +33,8 @@ export class AggenciesComponent implements OnInit {
     },
   ];
 
+  faqs: IFaq[];
+
   testimonials: ITestimonial[];
   devrelAgenciesCommunities: IPreferredPartners[];
   eventManagementCommunities: IPreferredPartners[];
@@ -40,22 +42,6 @@ export class AggenciesComponent implements OnInit {
   showFullDescriptionDevrelAgency: boolean[] = [];
   showFullDescriptionEventManagement: boolean[] = [];
   showFullDescriptionVendors: boolean[] = [];
-
-  questions = [
-    'Can I create multiple organizations or business pages which have communities under them?',
-    'What is the subscription / payment model?',
-    'Does Commudle have the option for custom dashboards and API’s?',
-    'How do you ensure that my communities rank up on search engines?',
-    'How do you ensure data privacy of the users and my clients?',
-  ];
-
-  answers = [
-    'Yes, Commudle has the features for creating an umbrella of communities for different businesses you manage and each can have a separate page and communities under it. You can run multiple technology based, geography or any other classification based global communities on Commudle.',
-    'You can purchase annual community subscriptions for individual chapters or in bulk exclusively for your business clients.',
-    'Yes, we can create custom dashboards or API’s for you to integrate in your existing dashboards as per requirements. The charges for these can be a part of your subscription plan.',
-    'All our pages with rich content are search engine optimized and we have dedicated experts who keep improving the strategy. Most of our existing active communities pages have top ranks on search engines.',
-    'Commudle is GDPR compliant and ISO 27001 certified. We take data privacy very seriously and understand its importance for businesses and have placed consents at multiple points on the platform so that the users know how their data will be processed and who will have access to it.',
-  ];
 
   constructor(private footerService: FooterService, private seoService: SeoService, private cmsService: CmsService) {}
 
@@ -66,6 +52,7 @@ export class AggenciesComponent implements OnInit {
     this.getDevrelAgenciesCommunities();
     this.getEventManagementCommunities();
     this.getVendorsCommunities();
+    this.setFaqs();
   }
 
   ngOnDestroy() {
@@ -132,5 +119,35 @@ export class AggenciesComponent implements OnInit {
 
   toggleShowFullDescriptionVendors(index: number): void {
     this.showFullDescriptionVendors[index] = !this.showFullDescriptionVendors[index];
+  }
+
+  setFaqs() {
+    this.faqs = [
+      {
+        question: 'Can I create multiple organizations or business pages which have communities under them?',
+        answer:
+          'Yes, Commudle has the features for creating an umbrella of communities for different businesses you manage and each can have a separate page and communities under it. You can run multiple technology based, geography or any other classification based global communities on Commudle.',
+      },
+      {
+        question: 'What is the subscription / payment model?',
+        answer:
+          'You can purchase annual community subscriptions for individual chapters or in bulk exclusively for your business clients.',
+      },
+      {
+        question: 'Does Commudle have the option for custom dashboards and API’s?',
+        answer:
+          'Yes, we can create custom dashboards or API’s for you to integrate in your existing dashboards as per requirements. The charges for these can be a part of your subscription plan.',
+      },
+      {
+        question: 'How do you ensure that my communities rank up on search engines?',
+        answer:
+          'All our pages with rich content are search engine optimized and we have dedicated experts who keep improving the strategy. Most of our existing active communities pages have top ranks on search engines.',
+      },
+      {
+        question: 'How do you ensure data privacy of the users and my clients?',
+        answer:
+          'Commudle is GDPR compliant and ISO 27001 certified. We take data privacy very seriously and understand its importance for businesses and have placed consents at multiple points on the platform so that the users know how their data will be processed and who will have access to it.',
+      },
+    ];
   }
 }
