75d4aec7af7f02348d65448820d8571663b452f5
Arshdeep Singh <arsh199820@gmail.com>
2024-10-25 14:05:46 +0530

    fix: expert page faqs

--- apps/commudle-admin/src/app/feature-modules/expert-program/components/experts-program/experts-program.component.ts
+++ apps/commudle-admin/src/app/feature-modules/expert-program/components/experts-program/experts-program.component.ts
@@ -4,7 +4,7 @@ import { SeoService } from 'apps/shared-services/seo.service';
 import { CmsService } from 'apps/shared-services/cms.service';
 import { IListingPageHeader } from 'apps/shared-models/listing-page-header.model';
 import { FooterService } from 'apps/commudle-admin/src/app/services/footer.service';
-
+import { IFaq } from '@commudle/shared-models';
 @Component({
   selector: 'commudle-experts-program',
   templateUrl: './experts-program.component.html',
@@ -14,28 +14,15 @@ export class ExpertsProgramComponent implements OnInit, OnDestroy {
   staticAssets = staticAssets;
   expertsProgramPageHeader: IListingPageHeader;
   richText: string;
+  faqs: IFaq[];
 
-  questions = [
-    'Can I apply for more than one expert badge?',
-    'How much time does it take to get onboarded as an expert?',
-    'Does filling the application form guarantee that I will get selected as an expert?',
-    'Is my blue tick or expert badge permanent?',
-    'Is this a paid program?',
-  ];
-
-  answers = [
-    'Yes, you can apply based on your expertise skillset',
-    'On submission of Expert Application Form, we will analyse it on multiple criteria including verification of the links and other details shared with us, this could take 1-2 weeks. ',
-    'The application form is your nomination for the Expert Program. Basis your input we will evaluate your profile. Make sure you keep your Commudle Profile updated even after submission.',
-    'A blue tick or an expert badge is subject to your activity on the platform. We have an activity criteria to meet which ensures that the expert badge and blue tick are present on your Commudle Profile.',
-    'Commudle does not charge any fee participating in this program. This program is designed to support and recognize techies enthusiastic to build and share their skills with the developer community.',
-  ];
   constructor(private seoService: SeoService, private cmsService: CmsService, private footerService: FooterService) {}
 
   ngOnInit(): void {
     this.footerService.changeFooterStatus(true);
     this.setMeta();
     this.getHeaderText();
+    this.setFaqs();
   }
 
   setMeta() {
@@ -61,5 +48,34 @@ export class ExpertsProgramComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.footerService.changeFooterStatus(false);
+  }
+
+  setFaqs() {
+    this.faqs = [
+      {
+        question: 'Can I apply for more than one expert badge?',
+        answer: 'Yes, you can apply based on your expertise skillset',
+      },
+      {
+        question: 'How much time does it take to get onboarded as an expert?',
+        answer:
+          'On submission of Expert Application Form, we will analyse it on multiple criteria including verification of the links and other details shared with us, this could take 1-2 weeks.',
+      },
+      {
+        question: 'Does filling the application form guarantee that I will get selected as an expert?',
+        answer:
+          'The application form is your nomination for the Expert Program. Basis your input we will evaluate your profile. Make sure you keep your Commudle Profile updated even after submission.',
+      },
+      {
+        question: 'Is my blue tick or expert badge permanent?',
+        answer:
+          'A blue tick or an expert badge is subject to your activity on the platform. We have an activity criteria to meet which ensures that the expert badge and blue tick are present on your Commudle Profile.',
+      },
+      {
+        question: 'Is this a paid program?',
+        answer:
+          'Commudle does not charge any fee participating in this program. This program is designed to support and recognize techies enthusiastic to build and share their skills with the developer community.',
+      },
+    ];
   }
 }
