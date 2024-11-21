b5dd8bb3fcde44057cefb9851bbfd5aec4ce7b6d
Prerna <rprerna2000@gmail.com>
2024-11-21 15:37:16 +0530

    style: add schema data for community newsletter

--- apps/commudle-admin/src/app/feature-modules/public-community/components/newsletters/newsletter/newsletter.component.ts
+++ apps/commudle-admin/src/app/feature-modules/public-community/components/newsletters/newsletter/newsletter.component.ts
@@ -4,6 +4,7 @@ import { ICommunity } from '@commudle/shared-models';
 import { NewsletterService } from 'apps/commudle-admin/src/app/services/newsletter.service';
 import { INewsletter } from 'apps/shared-models/newsletter.model';
 import { SeoService } from '@commudle/shared-services';
+import { environment } from '@commudle/shared-environments';
 
 @Component({
   selector: 'commudle-newsletter',
@@ -31,6 +32,7 @@ export class NewsletterComponent implements OnInit {
     this.newsletterService.getPShow(slug, parentId, 'Kommunity').subscribe((data) => {
       this.newsletter = data;
       this.setMeta();
+      this.setSchema();
     });
   }
 
@@ -40,5 +42,29 @@ export class NewsletterComponent implements OnInit {
       this.newsletter.brief_description,
       'https://commudle.com/assets/images/commudle-logo192.png',
     );
+  }
+
+  setSchema() {
+    const schemaForNewsletter = {
+      '@context': 'https://schema.org',
+      '@type': 'Article',
+      headline: this.newsletter.title,
+      url: environment.app_url + '/newsletters/' + this.newsletter.id,
+      author: {
+        '@type': 'Organization',
+        name: this.community.name,
+        url: environment.app_url + '/communities/' + this.community.slug,
+      },
+      publisher: {
+        '@type': 'Organization',
+        name: 'Commudle',
+        logo: {
+          '@type': 'ImageObject',
+          url: 'https://www.commudle.com/assets/images/commudle-logo-full.png',
+        },
+      },
+      datePublished: this.newsletter.created_at,
+    };
+    this.seoService.setSchema(schemaForNewsletter);
   }
 }
