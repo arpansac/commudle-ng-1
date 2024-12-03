d9f7458333729da577cffc23c9deb4bfeec6057b
Prerna <rprerna2000@gmail.com>
2024-12-03 15:23:39 +0530

    style: change path

--- apps/commudle-admin/src/app/app-shared-components/featured-experts-card/featured-experts-card.component.ts
+++ apps/commudle-admin/src/app/app-shared-components/featured-experts-card/featured-experts-card.component.ts
@@ -5,7 +5,7 @@ import { staticAssets } from 'apps/commudle-admin/src/assets/static-assets';
 import { MiniUserProfileModule } from 'apps/shared-modules/mini-user-profile/mini-user-profile.module';
 import { RouterModule } from '@angular/router';
 import { IFeaturedItems } from 'apps/shared-models/featured-items.model';
-import { UserPersonalConnectComponent } from '../user-personal-connect/user-personal-connect.component';
+import { UserPersonalConnectComponent } from 'apps/commudle-admin/src/app/app-shared-components/user-personal-connect/user-personal-connect.component';
 
 @Component({
   selector: 'commudle-featured-experts-card',
