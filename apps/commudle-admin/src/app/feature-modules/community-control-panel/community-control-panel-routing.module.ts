9bd4525f19f74904dc1edb8e06bf9f078fe60bb6
Prerna <rprerna2000@gmail.com>
2024-12-24 19:19:25 +0530

    style: alignment of links

--- apps/commudle-admin/src/app/feature-modules/community-control-panel/community-control-panel-routing.module.ts
+++ apps/commudle-admin/src/app/feature-modules/community-control-panel/community-control-panel-routing.module.ts
@@ -48,20 +48,20 @@ const routes = [
         path: 'notifications',
         component: CommunityAdminNotificationsComponent,
       },
-      {
-        path: 'forms',
-        component: CommunityFormsAndSurveysComponent,
-        children: [
-          {
-            path: '',
-            component: CommunityFormsListComponent,
-          },
-          {
-            path: 'surveys',
-            component: CommunitySurveysComponent,
-          },
-        ],
-      },
+      // {
+      //   path: 'forms',
+      //   component: CommunityFormsAndSurveysComponent,
+      //   children: [
+      //     {
+      //       path: '',
+      //       component: CommunityFormsListComponent,
+      //     },
+      //     {
+      //       path: 'surveys',
+      //       component: CommunitySurveysComponent,
+      //     },
+      //   ],
+      // },
       {
         path: 'edit',
         component: CommunityEditDetailsComponent,
@@ -135,6 +135,14 @@ const routes = [
       {
         path: 'team',
         component: CommunityTeamComponent,
+      },
+      {
+        path: 'forms',
+        component: CommunityFormsListComponent,
+      },
+      {
+        path: 'surveys',
+        component: CommunitySurveysComponent,
       },
       {
         path: 'channels',
