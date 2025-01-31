ce32841ee9fabf8a4935d75dc1221b22efa8236e
Prerna <rprerna2000@gmail.com>
2025-01-31 11:41:36 +0530

    style: listing page layout

---
+++ apps/commudle-admin/src/app/app-shared-components/listing-page-header/listing-page-header.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CmsService } from 'apps/shared-services/cms.service';
+import { IListingPageHeader } from 'apps/shared-models/listing-page-header.model';
+
+@Component({
+  selector: 'commudle-listing-page-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './listing-page-header.component.html',
+  styleUrls: ['./listing-page-header.component.scss'],
+})
+export class ListingPageHeaderComponent implements OnInit {
+  @Input() parentType: string;
+  header: IListingPageHeader;
+  richText: string;
+
+  constructor(private cmsService: CmsService) {}
+
+  ngOnInit(): void {
+    this.getHeaderText(this.parentType);
+  }
+
+  imageUrl(source: any) {
+    return this.cmsService.getImageUrl(source);
+  }
+
+  getHeaderText(parentType) {
+    this.cmsService.getDataBySlug(parentType).subscribe((data) => {
+      this.header = data;
+      this.richText = this.cmsService.getHtmlFromBlock(data);
+    });
+  }
+}
