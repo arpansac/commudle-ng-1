4f6c390a3da1c71ec2fb8b09ea83d75806feec81
Arshdeep Singh <arsh199820@gmail.com>
2025-02-06 13:37:33 +0530

    feat: enhance input component with additional properties and update campaign form setup

--- apps/commudle-admin/src/app/feature-modules/admin-campaign/components/campaign-form/campaign-form-order-setup/campaign-form-order-setup.component.ts
+++ apps/commudle-admin/src/app/feature-modules/admin-campaign/components/campaign-form/campaign-form-order-setup/campaign-form-order-setup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-
+import { faPlus, faXmark, faArrowRight, faFileImage } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'commudle-campaign-form-order-setup',
   templateUrl: './campaign-form-order-setup.component.html',
@@ -8,12 +9,77 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CampaignFormOrderSetupComponent implements OnInit {
   fragment: string;
-  constructor(private activatedRoute: ActivatedRoute) {}
+  campaignForm: FormGroup;
+  icons = {
+    faPlus,
+    faXmark,
+    faArrowRight,
+    faFileImage,
+  };
+  constructor(private activatedRoute: ActivatedRoute, private _fb: FormBuilder) {
+    this.campaignForm = this._fb.group({
+      name: ['', Validators.required],
+      email_address: ['', [Validators.required, Validators.email]],
+      company_name: ['', Validators.required],
+      campaign_name: ['', Validators.required],
+      start_time: ['', Validators.required],
+      end_time: ['', Validators.required],
+      budget: [0, Validators.required],
+      campaign_assets: this._fb.array([this.createCampaignAsset()]),
+    });
+  }
+
+  createCampaignAsset(): FormGroup {
+    return this._fb.group({
+      image: [null, Validators.required],
+      headline: ['', Validators.required],
+      url: ['', [Validators.required, Validators.pattern(/^(https?:\/\/)[\w.-]+(\.[a-zA-Z]{2,})+([/?#].*)?$/)]],
+    });
+  }
+
+  get campaignAssets(): FormArray {
+    return this.campaignForm.get('campaign_assets') as FormArray;
+  }
+
+  addCampaignAsset() {
+    this.campaignAssets.push(this.createCampaignAsset());
+  }
+
+  removeCampaignAsset(index: number) {
+    this.campaignAssets.removeAt(index);
+  }
 
   ngOnInit() {
     this.activatedRoute.fragment.subscribe((fragment) => {
       this.fragment = fragment || '';
-      console.log(this.fragment); // Output: 'user-info' if clicked
+      console.log(this.fragment);
     });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onFileChange(event: any, index: number) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.campaignAssets.at(index).patchValue({ image: file });
+    }
+  }
+
+  handleInput(value: string | number, key: string) {
+    this.campaignForm.patchValue({ [key]: value });
+    console.log(this.campaignForm.value);
+  }
+
+  handleArrayFormInput(value: string | number, key: string, index: number, formArrayName: string) {
+    const formArray = this.campaignForm.get(formArrayName) as FormArray;
+
+    if (formArray && formArray.controls[index]) {
+      formArray.at(index).patchValue({ [key]: value });
+      console.log(this.campaignForm.value); // Debugging output
+    } else {
+      console.error(`Invalid index ${index} for form array ${formArrayName}`);
+    }
+  }
+  updateCampaign() {
+    console.log(this.campaignForm.value);
   }
 }
--- libs/shared/components/src/lib/components/input/input.component.ts
+++ libs/shared/components/src/lib/components/input/input.component.ts
@@ -7,10 +7,18 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class InputComponent {
   @Input() placeholder = 'Enter text';
-  @Output() valueChange = new EventEmitter<string>();
-  inputValue = '';
+  @Input() type: 'text' | 'number' | 'datetime-local' = 'text';
+  @Input() id: string;
+  @Input() name: string;
+  @Output() valueChange = new EventEmitter<string | number>();
+  inputValue: string | number = '';
 
-  onInputChange() {
+  onInputChange(event: any) {
+    if (this.type === 'number') {
+      this.inputValue = event.target.value ? Number(event.target.value) : '';
+    } else {
+      this.inputValue = event.target.value;
+    }
     this.valueChange.emit(this.inputValue);
   }
 }
