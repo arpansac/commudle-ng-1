1091ea5b320af2e88fe9384c188dd3f493435697
Arshdeep Singh <arsh199820@gmail.com>
2025-02-11 23:31:46 +0530

    feat: enhance campaign list with detailed view and dialog for campaign details

--- apps/shared-components/campaign-list/campaign-list.component.ts
+++ apps/shared-components/campaign-list/campaign-list.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { ICampaign, ECampaignStatus } from '@commudle/shared-models';
 import { CampaignService, ToastrService } from '@commudle/shared-services';
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { NbDialogService } from '@commudle/theme';
 import moment from 'moment';
 
 @Component({
@@ -16,12 +17,24 @@ export class CampaignListComponent {
   icons = { faEdit };
   ECampaignStatus = ECampaignStatus;
 
-  constructor(private campaignService: CampaignService, private toasterService: ToastrService) {}
+  constructor(
+    private campaignService: CampaignService,
+    private toasterService: ToastrService,
+    private dialogService: NbDialogService,
+  ) {}
 
   updateStatus(event, campaignId) {
     this.campaignService.sysAdminUpdateStatus(campaignId, event.target.value).subscribe((res) => {
       if (res) {
         this.toasterService.successDialog('Campaign status updated successfully');
+      }
+    });
+  }
+
+  openPopup(dialog, campaignId) {
+    this.campaignService.fetchCampaign(campaignId).subscribe((campaign) => {
+      if (campaign) {
+        this.dialogService.open(dialog, { context: campaign });
       }
     });
   }
