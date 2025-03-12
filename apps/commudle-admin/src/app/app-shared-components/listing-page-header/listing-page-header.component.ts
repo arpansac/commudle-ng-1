f1e51bc40996bdeac59b7175bea4f8cb841d000f
Prerna <rprerna2000@gmail.com>
2025-03-12 20:25:48 +0530

    style: sanity content color

--- apps/commudle-admin/src/app/app-shared-components/listing-page-header/listing-page-header.component.ts
+++ apps/commudle-admin/src/app/app-shared-components/listing-page-header/listing-page-header.component.ts
@@ -2,11 +2,12 @@ import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CmsService } from 'apps/shared-services/cms.service';
 import { IListingPageHeader } from 'apps/shared-models/listing-page-header.model';
+import { SharedPipesModule } from 'apps/shared-pipes/pipes.module';
 
 @Component({
   selector: 'commudle-listing-page-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SharedPipesModule],
   templateUrl: './listing-page-header.component.html',
   styleUrls: ['./listing-page-header.component.scss'],
 })
