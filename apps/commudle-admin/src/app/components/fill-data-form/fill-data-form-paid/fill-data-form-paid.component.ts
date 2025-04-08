593db530279f774b5de215f5946098d10a9fd0b1
Arshdeep Singh <arsh199820@gmail.com>
2025-04-08 15:08:02 +0530

    fix: update order creation parameters in Razorpay service call

--- apps/commudle-admin/src/app/components/fill-data-form/fill-data-form-paid/fill-data-form-paid.component.ts
+++ apps/commudle-admin/src/app/components/fill-data-form/fill-data-form-paid/fill-data-form-paid.component.ts
@@ -647,7 +647,7 @@ export class FillDataFormPaidComponent implements OnInit, OnDestroy, AfterViewIn
       this.checkEventTicketOrder(this.dataFormEntity.entity_id);
       return;
     }
-    this.razorpayService.createOrFindOrder(orderDetails, etoId).subscribe(
+    this.razorpayService.createOrFindOrder(orderDetails, { eto_id: etoId }).subscribe(
       (data: IRazorpayOrder) => {
         this.razorPaySubmit(data);
       },
