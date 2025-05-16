81319ae3cdab986ad7f0cf8a86d6c7b4d8fdc3e5
Prerna <rprerna2000@gmail.com>
2025-05-17 00:45:16 +0530

    style: user-context-menu desktop

--- apps/commudle-admin/src/app/components/navbar-user-context-menu/navbar-user-context-menu.component.ts
+++ apps/commudle-admin/src/app/components/navbar-user-context-menu/navbar-user-context-menu.component.ts
@@ -1,36 +1,30 @@
-import { Component, Input, OnInit, OnDestroy, HostListener } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy, TemplateRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { ICurrentUser } from 'apps/shared-models/current_user.model';
+import { NbDialogRef, NbDialogService } from '@commudle/theme';
 
 @Component({
   selector: 'commudle-navbar-user-context-menu',
   templateUrl: './navbar-user-context-menu.component.html',
   styleUrls: ['./navbar-user-context-menu.component.scss'],
 })
-export class NavbarUserContextMenuComponent implements OnInit, OnDestroy {
+export class NavbarUserContextMenuComponent implements OnInit {
   @Input() currentUser: ICurrentUser;
   showUserContextMenu = true;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private dialogService: NbDialogService) {}
 
-  ngOnInit(): void {
-    document.addEventListener('click', this.handleClickOutside.bind(this));
-  }
-
-  ngOnDestroy(): void {
-    document.removeEventListener('click', this.handleClickOutside.bind(this));
-  }
+  ngOnInit(): void {}
 
   logout(): void {
     this.router.navigate(['/logout']);
     this.showUserContextMenu = false;
   }
 
-  handleClickOutside(event: MouseEvent): void {
-    const target = event.target as HTMLElement;
-    const contextMenu = document.querySelector('.user-profile-dropdown');
-    if (contextMenu && !contextMenu.contains(target)) {
-      this.showUserContextMenu = false;
-    }
+  openConfirmDialogBox(dialog: TemplateRef<any>) {
+    this.dialogService.open(dialog, {
+      closeOnBackdropClick: true,
+      closeOnEsc: true,
+    });
   }
 }
