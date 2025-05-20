31da9f2594e450484ead4857358e29bd8777d92d
Prerna <rprerna2000@gmail.com>
2025-05-20 13:29:38 +0530

    style: fix custom pgae meta data of orgs and community

--- apps/commudle-admin/src/app/feature-modules/public-community-groups/components/community-group-custom-page/community-group-custom-page.component.ts
+++ apps/commudle-admin/src/app/feature-modules/public-community-groups/components/community-group-custom-page/community-group-custom-page.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { EDbModels } from '@commudle/shared-models';
 import { CustomPageService } from 'apps/commudle-admin/src/app/services/custom-page.service';
 import { ICustomPage } from 'apps/shared-models/custom-page.model';
+import { SeoService } from 'apps/shared-services/seo.service';
 import { Subscription, combineLatest } from 'rxjs';
 
 @Component({
@@ -15,7 +16,11 @@ export class CommunityGroupCustomPageComponent implements OnInit, OnDestroy {
   page: ICustomPage;
   EDbModels = EDbModels;
 
-  constructor(private activatedRoute: ActivatedRoute, private customPageService: CustomPageService) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private customPageService: CustomPageService,
+    private seoService: SeoService,
+  ) {}
 
   ngOnInit() {
     combineLatest([this.activatedRoute.params, this.activatedRoute.parent.data]).subscribe(([params, data]) => {
@@ -29,11 +34,20 @@ export class CommunityGroupCustomPageComponent implements OnInit, OnDestroy {
     this.subscriptions.push(
       this.customPageService.getPShow(pageSlug, communityGroupId, EDbModels.COMMUNITY_GROUP).subscribe((data) => {
         this.page = data;
+        this.setMeta();
       }),
     );
   }
 
   ngOnDestroy() {
     this.subscriptions.forEach((subscription: Subscription) => subscription.unsubscribe());
+  }
+
+  setMeta() {
+    this.seoService.setTags(
+      this.page.title,
+      this.seoService.removeHtmlTags(this.page.description),
+      'https://commudle.com/assets/images/commudle-logo192.png',
+    );
   }
 }
