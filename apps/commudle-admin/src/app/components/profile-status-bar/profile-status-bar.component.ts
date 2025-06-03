1a06f85914cacf3e04d4600f58f207be34c46a58
Prerna <rprerna2000@gmail.com>
2025-06-03 22:53:35 +0530

    fix: destroy subscription on onDestroy

--- apps/commudle-admin/src/app/components/profile-status-bar/profile-status-bar.component.ts
+++ apps/commudle-admin/src/app/components/profile-status-bar/profile-status-bar.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { GoogleTagManagerService } from 'apps/commudle-admin/src/app/services/google-tag-manager.service';
 import { ProfileStatusBarService } from 'apps/commudle-admin/src/app/services/profile-status-bar.service';
 import { StepperService } from 'apps/commudle-admin/src/app/services/stepper.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-profile-status-bar',
   templateUrl: './profile-status-bar.component.html',
   styleUrls: ['./profile-status-bar.component.scss'],
 })
-export class ProfileStatusBarComponent implements OnInit {
+export class ProfileStatusBarComponent implements OnInit, OnDestroy {
   value = 0;
+  private subscription: Subscription;
 
   constructor(
     private stepperService: StepperService,
@@ -22,22 +24,22 @@ export class ProfileStatusBarComponent implements OnInit {
 
   ngOnInit(): void {
     this.stepperService.getProfilePercentage();
-    this.stepperService.profileCompletePercentage$.subscribe((percentage) => {
+    this.subscription = this.stepperService.profileCompletePercentage$.subscribe((percentage) => {
       this.value = percentage;
       if (percentage >= 90) {
         this.profileStatusBarService.changeProfileBarStatus(false);
       } else {
         this.profileStatusBarService.changeProfileBarStatus(true);
-        if (this.activatedRoute.snapshot.queryParams['show-profile-complete-popup'] === 'true') {
-          this.stepperService.showStepper();
-        }
       }
     });
   }
 
   showStepper() {
-    this.router.navigate(['/user-profile-complete/step-one']);
-    this.stepperService.showStepper();
     this.gtm.dataLayerPushEvent('click_open_complete_profile_popup', {});
+    this.router.navigate(['/user-profile-complete/step-one']);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
