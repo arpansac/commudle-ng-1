55441555fa991f841c073a836f83b730ad9ce91c
Arshdeep Singh <arsh199820@gmail.com>
2025-06-09 18:25:59 +0530

    feat(fill-data-form-confirmation): update event details heading and styling

--- apps/commudle-admin/src/app/app-shared-components/event-cards/event-registered-card/event-registered-card.component.ts
+++ apps/commudle-admin/src/app/app-shared-components/event-cards/event-registered-card/event-registered-card.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { IEvent } from '@commudle/shared-models';
+import { IEvent, IUser } from '@commudle/shared-models';
 import * as moment from 'moment';
+import { EventsService } from 'apps/commudle-admin/src/app/services/events.service';
+import {
+  faArrowUpRightFromSquare,
+  faCalendarDays,
+  faCalendarPlus,
+  faLocationDot,
+  faShareNodes,
+} from '@fortawesome/free-solid-svg-icons';
+import { faCircleCheck } from '@fortawesome/free-regular-svg-icons';
+import { ShareService } from '@commudle/shared-services';
+import { environment } from '@commudle/shared-environments';
 
 @Component({
   selector: 'commudle-event-registered-card',
@@ -10,6 +21,33 @@ import * as moment from 'moment';
 export class EventRegisteredCardComponent implements OnInit {
   @Input() event: IEvent;
   moment = moment;
+  interestedUsers: IUser[];
+  interestedUsersCount: number;
+  eventUrl: string;
+  readonly icons = {
+    faCalendarDays,
+    faLocationDot,
+    faCalendarPlus,
+    faShareNodes,
+    faArrowUpRightFromSquare,
+    faCircleCheck,
+  };
+  constructor(private eventService: EventsService, private shareService: ShareService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.fetchInterestedMembers();
+  }
+
+  fetchInterestedMembers() {
+    this.eventService.pGetEventsInterestedMembers(this.event.id).subscribe((res) => {
+      this.interestedUsers = res.users;
+      this.interestedUsersCount = res.total_count;
+    });
+  }
+
+  shareEvent() {
+    const eventUrl = `${environment.app_url}/communities/${this.event.kommunity.id}/events/${this.event.slug}`;
+    const shareText = `Check out this event: ${this.event.name}`;
+    this.shareService.shareContent(eventUrl, this.event.name, shareText, eventUrl);
+  }
 }
