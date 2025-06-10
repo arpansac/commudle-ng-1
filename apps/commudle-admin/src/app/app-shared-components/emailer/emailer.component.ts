9010d646061985cb79f870ea4b4d7008ddf0ec3f
Aditya Gulati <gulati.ad@gmail.com>
2025-06-10 17:09:30 +0530

    style: improve table styling in community event by making the scrollbar automatic

--- apps/commudle-admin/src/app/app-shared-components/emailer/emailer.component.ts
+++ apps/commudle-admin/src/app/app-shared-components/emailer/emailer.component.ts
@@ -287,14 +287,15 @@ export class EmailerComponent implements OnInit, OnDestroy {
   getAllEvents(): Promise<void> {
     return new Promise((resolve, reject) => {
       let page = 1;
+      const count = 10;
       const allEvents = [];
 
       const getNextPage = () => {
-        this.eventsService.communityEventsForEmail(this.community.id, page, 10, '', [], true).subscribe(
+        this.eventsService.communityEventsForEmail(this.community.id, page, count, '', [], true).subscribe(
           (data) => {
             allEvents.push(...data.values);
 
-            if (allEvents.length < data.total && data.values.length === 10) {
+            if (allEvents.length < data.total && data.values.length === count) {
               page++;
               getNextPage();
             } else {
