29cbbae077626b58be5cc923104afdbae767a346
Arshdeep Singh <arsh199820@gmail.com>
2025-06-10 15:01:53 +0530

    feat(profile-completion): implement unsubscribe logic in ProfileCompletionProgress component

--- libs/shared/components/src/lib/components/user/profile-completion-progress/profile-completion-progress.component.ts
+++ libs/shared/components/src/lib/components/user/profile-completion-progress/profile-completion-progress.component.ts
@@ -1,22 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AppUsersService } from '@commudle/shared-services';
 import { IProfileCompletionStatus } from '@commudle/shared-models';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'commudle-profile-completion-progress',
   templateUrl: './profile-completion-progress.component.html',
   styleUrls: ['./profile-completion-progress.component.scss'],
 })
-export class ProfileCompletionProgressComponent implements OnInit {
+export class ProfileCompletionProgressComponent implements OnInit, OnDestroy {
   profileCompletionPercentage = 0;
+
+  private destroy$ = new Subject<void>();
 
   constructor(private appUsersService: AppUsersService) {}
 
   ngOnInit(): void {
-    this.appUsersService.profileCompletionStatus$.subscribe((status: IProfileCompletionStatus) => {
-      if (status) {
-        this.profileCompletionPercentage = status.completion_percentage;
-      }
-    });
+    this.appUsersService.profileCompletionStatus$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((status: IProfileCompletionStatus) => {
+        if (status) {
+          this.profileCompletionPercentage = status.completion_percentage;
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
