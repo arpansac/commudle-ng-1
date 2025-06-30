6b3098015d429ba33d14dcab814bd3a30567bd52
Aditya Gulati <gulati.ad@gmail.com>
2025-06-30 22:20:48 +0530

    refactor: removing unused imports and styles from show more builds component

--- apps/commudle-admin/src/app/feature-modules/community-builds/components/show-more-builds.component.ts
+++ apps/commudle-admin/src/app/feature-modules/community-builds/components/show-more-builds.component.ts
@@ -1,19 +1,17 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { AppUsersService } from 'apps/commudle-admin/src/app/services/app-users.service';
 import { IUser, ICommunityBuild } from '@commudle/shared-models';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'commudle-show-more-builds',
   templateUrl: './show-more-builds.component.html',
   styleUrls: ['./show-more-builds.component.scss'],
 })
-export class ShowMoreBuildsComponent implements OnInit, OnDestroy {
+export class ShowMoreBuildsComponent implements OnInit {
   @Input() user: IUser;
   @Input() curBuild: ICommunityBuild;
 
   builds: ICommunityBuild[] = [];
-  subscriptions: Subscription[] = [];
 
   constructor(private appUsersService: AppUsersService) {}
 
@@ -21,15 +19,9 @@ export class ShowMoreBuildsComponent implements OnInit, OnDestroy {
     this.getBuilds();
   }
 
-  ngOnDestroy(): void {
-    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
-  }
-
   getBuilds(): void {
-    if (this.user?.username && this.curBuild?.id) {
-      this.appUsersService.communityBuilds(this.user.username).subscribe((value) => {
-        this.builds = value.community_builds.filter((build) => build.id !== this.curBuild.id).reverse();
-      });
-    }
+    this.appUsersService.communityBuilds(this.user.username).subscribe((value) => {
+      this.builds = value.community_builds.filter((build) => build.id !== this.curBuild.id).reverse();
+    });
   }
 }
