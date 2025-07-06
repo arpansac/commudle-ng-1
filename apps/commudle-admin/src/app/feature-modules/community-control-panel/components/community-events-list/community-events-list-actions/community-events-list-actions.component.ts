c69dd041bf9a7149daf62d33ed08795cbb67b92d
Django-07 <bhavisya.aka.ratna@gmail.com>
2025-07-06 17:12:07 +0530

    fix(event-list): redirected page to registrations when status is open

--- apps/commudle-admin/src/app/feature-modules/community-control-panel/components/community-events-list/community-events-list-actions/community-events-list-actions.component.ts
+++ apps/commudle-admin/src/app/feature-modules/community-control-panel/components/community-events-list/community-events-list-actions/community-events-list-actions.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { IEvent } from '@commudle/shared-models';
 import { NbDialogService } from '@commudle/theme';
+import { EEventStatuses } from 'apps/shared-models/enums/event_statuses.enum';
 
 @Component({
   selector: 'commudle-community-events-list-actions',
@@ -10,9 +11,17 @@ import { NbDialogService } from '@commudle/theme';
 export class CommunityEventsListActionsComponent {
   @Input() value: string | number;
   @Input() eventData: IEvent;
+  EEventStatuses = EEventStatuses;
   constructor(private dialogBoxService: NbDialogService) {}
-
   openCloneEventWindow(dialogBox) {
     this.dialogBoxService.open(dialogBox);
   }
+  // gotoRegistrations(eventData: any): void{
+  //   if(eventData === EEventStatuses.OPEN){
+  //     this.router.navigate(['event-dashboard-test-sidekiq-registrations',eventData.slug])
+  //   }
+  //   else{
+  //     this.router.navigate(['event-dashboard',eventData.slug])
+  //   }
+  // }
 }
