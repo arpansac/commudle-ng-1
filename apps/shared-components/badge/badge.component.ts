1c1e8a045dda9196a0c7f3cb09259b3aa926ff82
anishagarg327 <anishagarg.12092006@gmail.com>
2025-07-09 11:12:58 +0530

    fix(event-list): changed badge

--- apps/shared-components/badge/badge.component.ts
+++ apps/shared-components/badge/badge.component.ts
@@ -11,12 +11,12 @@ import { NbIconModule } from '@commudle/theme';
 })
 export class BadgeComponent implements OnInit {
   @Input() text;
-  @Input() fontSize; //can be 'small or regular'
-  @Input() color = 'com-bg-Bright-Gray';
+  @Input() fontSize: 'small' | 'x-small' | 'xx-small' = 'x-small'; //can be 'small or regular'
+  @Input() color = 'com-bg-Red';
   @Input() fontColor = 'com-text-tWhite';
   @Input() nbIcon;
   @Input() dotMode;
-  @Input() position;
+  @Input() position: string = '';
   @Input() borderRadius: 'rectangle' | 'semi-round' | 'round' = 'rectangle';
 
   // bg;
