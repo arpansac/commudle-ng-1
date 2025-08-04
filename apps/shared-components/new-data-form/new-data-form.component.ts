8f2e41df17883b83b58f5caac0cdec588c94cd4e
Django-07 <bhavisya.aka.ratna@gmail.com>
2025-08-05 00:50:24 +0530

    fix(question-card): changed design of question card

--- apps/shared-components/new-data-form/new-data-form.component.ts
+++ apps/shared-components/new-data-form/new-data-form.component.ts
@@ -48,6 +48,11 @@ export class NewDataFormComponent implements OnInit {
 
   menuItem = [
     {
+      title: 'Add Description',
+      icon: 'file-text-outline',
+      // data: { action: 'addDescription' },
+    },
+    {
       title: 'Add Question Below',
       icon: 'plus-circle-outline',
     },
@@ -260,6 +265,9 @@ export class NewDataFormComponent implements OnInit {
       )
       .subscribe((menu) => {
         switch (menu.title) {
+          case 'Add Description':
+            this.toggleDescriptionField(this.questionContextMenuIndex);
+            break;
           case 'Add Question Below':
             this.addQuestionButtonClick(this.questionContextMenuIndex + 1);
             break;
