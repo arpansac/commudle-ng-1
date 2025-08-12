0d4cca4f6c664299d2d2d2c881be0527825c8177
Prerna <rprerna2000@gmail.com>
2025-08-12 21:43:13 +0530

    refactor: add location in event form and hackathon form

--- apps/shared-components/user-details-form/user-details-form.component.ts
+++ apps/shared-components/user-details-form/user-details-form.component.ts
@@ -1,10 +1,11 @@
 import { KeyValue } from '@angular/common';
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { EDomain, EExperienceLevel, IHackathonUserResponse, IUser } from '@commudle/shared-models';
 import { faFileImage } from '@fortawesome/free-solid-svg-icons';
 import { Subject, takeUntil } from 'rxjs';
 import { AuthService, ToastrService } from '@commudle/shared-services';
+import { GooglePlacesAutocompleteService } from 'apps/commudle-admin/src/app/services/google-places-autocomplete.service';
 @Component({
   selector: 'commudle-user-details-form',
   templateUrl: './user-details-form.component.html',
@@ -26,7 +27,14 @@ export class UserDetailsFormComponent implements OnInit, OnDestroy {
   EDomain = EDomain;
   private destroy$ = new Subject<void>();
 
-  constructor(private authWatchService: AuthService, private fb: FormBuilder, private toastLogService: ToastrService) {}
+  @ViewChild('autocompleteInput', { static: true }) autocompleteInput: ElementRef;
+
+  constructor(
+    private authWatchService: AuthService,
+    private fb: FormBuilder,
+    private toastLogService: ToastrService,
+    private googlePlacesAutocompleteService: GooglePlacesAutocompleteService,
+  ) {}
 
   ngOnInit(): void {
     this.authWatchService.currentUser$.pipe(takeUntil(this.destroy$)).subscribe((data) => {
@@ -34,6 +42,10 @@ export class UserDetailsFormComponent implements OnInit, OnDestroy {
       this.uploadedProfilePicture = this.currentUser.avatar;
       this.userForm = this.createForm(this.userFormDetails);
     });
+  }
+
+  ngAfterViewInit(): void {
+    this.initAutocomplete();
   }
 
   ngOnDestroy(): void {
@@ -113,4 +125,16 @@ export class UserDetailsFormComponent implements OnInit, OnDestroy {
   originalOrder = (a: KeyValue<string, any>, b: KeyValue<string, any>): number => {
     return 0;
   };
+
+  initAutocomplete() {
+    const addressInput = document.getElementById('addressInput') as HTMLInputElement;
+    this.googlePlacesAutocompleteService.initAutocomplete(addressInput);
+    this.googlePlacesAutocompleteService.placeChanged.subscribe((place: google.maps.places.PlaceResult) => {
+      this.onLocationPlaceSelected(place);
+    });
+  }
+
+  onLocationPlaceSelected(place: google.maps.places.PlaceResult) {
+    this.userForm.get('location').setValue(place.formatted_address);
+  }
 }
