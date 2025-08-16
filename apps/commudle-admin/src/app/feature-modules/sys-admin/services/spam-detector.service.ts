12304be33c2ca13275217bd1e191a32a707f2d9c
Prerna <rprerna2000@gmail.com>
2025-08-16 21:13:52 +0530

    style: remove console from service

--- apps/commudle-admin/src/app/feature-modules/sys-admin/services/spam-detector.service.ts
+++ apps/commudle-admin/src/app/feature-modules/sys-admin/services/spam-detector.service.ts
@@ -34,7 +34,6 @@ export class SpamDetectorService {
   }
 
   updateSpamDetector(value, id): Observable<ISpamDetector> {
-    console.log(typeof value, typeof id, 'value, id');
     return this.http.put<ISpamDetector>(this.apiRoutesService.getRoute(API_ROUTES.SPAM_DETECTOR.UPDATE), {
       is_spam_decision: value,
       spam_detector_id: id,
