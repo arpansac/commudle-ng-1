5a0d6eccc32889243136c842efd5245b733b5691
Anshulgupta280104 <anshulgupta282004@gmail.com>
2025-08-21 17:13:09 +0530

    fix: added insta icon

--- apps/commudle-admin/src/app/components/footer/footer.component.ts
+++ apps/commudle-admin/src/app/components/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { faXTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faXTwitter, faYoutube, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { FooterService } from 'apps/commudle-admin/src/app/services/footer.service';
 import { AwsS3Bucket, staticAssets } from 'apps/commudle-admin/src/assets/static-assets';
 import { Observable } from 'rxjs';
@@ -14,6 +14,7 @@ export class FooterComponent {
   faYoutube = faYoutube;
   awsS3Bucket = AwsS3Bucket;
   faXTwitter = faXTwitter;
+  faInstagram = faInstagram;
 
   showFooter$: Observable<boolean> = this.footerService.footerStatus$;
   showMiniFooter$: Observable<boolean> = this.footerService.miniFooterStatus$;
