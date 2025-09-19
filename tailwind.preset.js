5f09f0f40702b4c4c06163ca827603e988aa8174
Prerna <rprerna2000@gmail.com>
2025-09-19 10:59:03 +0530

    style: change the variable name to custom color

--- tailwind.preset.js
+++ tailwind.preset.js
@@ -88,7 +88,7 @@ module.exports = {
         'gray-800': 'var(--color-gray-800)',
         'gray-600': 'var(--color-gray-600)',
         'gray-500-opacity-50': 'var(--color-gray-500-opacity-50)',
-        'gray-1000': 'var(--color-gray-1000)',
+        'gray-custom-1': 'var(--color-gray-custom-1)',
         'Bright-Gray-opacity-60': 'var(--color-Bright-Gray-opacity-60)',
         'Bright-Gray-opacity-30': 'var(--color-Bright-Gray-opacity-30)',
         'Ghost-White': 'var(--color-Ghost-White)',
