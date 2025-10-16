98d87f02b805eed020083d2909cbf583ea106b4d
Prerna <rprerna2000@gmail.com>
2025-10-16 18:48:45 +0530

    style: user-mutual-follower

--- libs/shared/models/src/lib/user.model.ts
+++ libs/shared/models/src/lib/user.model.ts
@@ -63,7 +63,10 @@ export interface IUser {
   user_domain: string;
   goals: string[];
   blocked: boolean;
-  mutual_followee: string;
+  mutual_followee: {
+    first_three: IUser[];
+    remaining_count: number;
+  };
 }
 
 export interface IUserSearch extends IUser {
