23dee8412d96e095537ad6031e10a7a2daf2f429
Prerna <rprerna2000@gmail.com>
2025-10-30 22:42:00 +0530

    style: hiring look work tags

--- apps/shared-modules/mini-user-profile/components/profile-cards/user-profile-card-medium/user-profile-card-medium.component.ts
+++ apps/shared-modules/mini-user-profile/components/profile-cards/user-profile-card-medium/user-profile-card-medium.component.ts
@@ -11,6 +11,7 @@ export class UserProfileCardMediumComponent implements OnInit {
   @Input() maxNameLength = 50;
   @Input() maxDesignationLength = 50;
   @Input() openInNewTab = false;
+  @Input() showHiringLookingTags = false;
 
   constructor() {}
 
