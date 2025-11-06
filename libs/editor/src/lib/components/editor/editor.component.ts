b545f09e28f3038cc8ce7145226c6d2b65e9e1e8
Prerna <rprerna2000@gmail.com>
2025-11-06 15:37:29 +0530

    style: add auto focus in reply comment

--- libs/editor/src/lib/components/editor/editor.component.ts
+++ libs/editor/src/lib/components/editor/editor.component.ts
@@ -157,4 +157,10 @@ export class EditorComponent implements OnInit, OnDestroy, OnChanges {
   uploadAttachment(event) {
     this.uploadImages.emit(event.target.files);
   }
+
+  public focus(): void {
+    if (this.editor && !this.editor.isDestroyed) {
+      this.editor.commands.focus();
+    }
+  }
 }
