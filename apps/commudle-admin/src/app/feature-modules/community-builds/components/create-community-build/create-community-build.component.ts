5f848d2948ec4c1b59907468498e5f0e9e6fa216
Prerna <rprerna2000@gmail.com>
2025-12-03 15:30:35 +0530

    fix: live project pre-checked

--- apps/commudle-admin/src/app/feature-modules/community-builds/components/create-community-build/create-community-build.component.ts
+++ apps/commudle-admin/src/app/feature-modules/community-builds/components/create-community-build/create-community-build.component.ts
@@ -68,7 +68,7 @@ export class CreateCommunityBuildComponent implements OnInit, OnDestroy {
   communityBuildForm;
   communityBuildUpdateForm;
   moment = moment;
-  hasOpenSourceLink = false;
+  hasLiveProjectLink = false;
   editBuildForm = false;
   EInvitationStatus = EInvitationStatus;
 
@@ -278,6 +278,8 @@ export class CreateCommunityBuildComponent implements OnInit, OnDestroy {
   prefillCommunityBuild() {
     this.communityBuildForm.patchValue(this.cBuild);
     this.setBuildType();
+
+    this.hasLiveProjectLink = !!this.cBuild.live_app_link;
 
     for (const img of this.cBuild.images) {
       this.uploadedImagesFiles.push({
