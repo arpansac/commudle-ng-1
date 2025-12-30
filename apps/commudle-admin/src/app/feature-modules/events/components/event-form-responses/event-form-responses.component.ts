90b2891d23f641fcafa921bc58e95da19557e95e
Arshdeep Singh <arsh199820@gmail.com>
2025-12-30 11:35:39 +0530

    fix(event-form): move table column setup to correct lifecycle hook

--- apps/commudle-admin/src/app/feature-modules/events/components/event-form-responses/event-form-responses.component.ts
+++ apps/commudle-admin/src/app/feature-modules/events/components/event-form-responses/event-form-responses.component.ts
@@ -221,7 +221,6 @@ export class EventFormResponsesComponent implements OnInit, OnDestroy, AfterView
       .subscribe((data) => {
         this.dataForm = data;
         this.questions = this.dataForm.questions;
-        this.setupTableColumns();
       });
 
     this.getResponses();
@@ -241,6 +240,7 @@ export class EventFormResponsesComponent implements OnInit, OnDestroy, AfterView
       .getEventDataFormEntityGroup(this.eventDataFormEntityGroupId)
       .subscribe((data) => {
         this.eventDataFormEntityGroup = data;
+        this.setupTableColumns();
         if (
           this.eventDataFormEntityGroup.registration_type.name === RegistrationTypeNames.SPEAKER &&
           fetchEventLocationTrack
