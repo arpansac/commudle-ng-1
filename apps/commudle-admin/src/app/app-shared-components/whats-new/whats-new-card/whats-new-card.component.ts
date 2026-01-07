12f3fb7ec2a0445b82ddd3f1bb75a4e27eb88c6c
Prerna <rprerna2000@gmail.com>
2026-01-07 20:07:54 +0530

    style: whats new popup design

--- apps/commudle-admin/src/app/app-shared-components/whats-new/whats-new-card/whats-new-card.component.ts
+++ apps/commudle-admin/src/app/app-shared-components/whats-new/whats-new-card/whats-new-card.component.ts
@@ -2,11 +2,12 @@ import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IWhatsNew } from 'apps/shared-models/whats-new.model';
 import { CmsService } from 'apps/shared-services/cms.service';
+import { BadgeComponent } from 'apps/shared-components/badge/badge.component';
 
 @Component({
   selector: 'commudle-whats-new-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BadgeComponent],
   templateUrl: './whats-new-card.component.html',
   styleUrls: ['./whats-new-card.component.scss'],
 })
@@ -17,5 +18,9 @@ export class WhatsNewCardComponent implements OnInit {
 
   ngOnInit(): void {
     this.richText = this.cmsService.getHtmlFromBlock(this.update);
+  }
+
+  imageUrl(source: any) {
+    return this.cmsService.getImageUrl(source);
   }
 }
--- apps/shared-models/whats-new.model.ts
+++ apps/shared-models/whats-new.model.ts
@@ -1,5 +1,8 @@
+import { SanityImageObject } from '@sanity/image-url/lib/types/types';
+
 export interface IWhatsNew {
   title: string;
   date: string;
   content: any;
+  image?: SanityImageObject;
 }
