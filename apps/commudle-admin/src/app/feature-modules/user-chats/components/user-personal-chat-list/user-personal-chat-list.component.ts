9a974931d3b30d3e1456a71379411871dfec93c3
Prerna <rprerna2000@gmail.com>
2026-04-08 23:32:56 +0530

    style: header and list of chat window and diable footer

--- apps/commudle-admin/src/app/feature-modules/user-chats/components/user-personal-chat-list/user-personal-chat-list.component.ts
+++ apps/commudle-admin/src/app/feature-modules/user-chats/components/user-personal-chat-list/user-personal-chat-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LayoutService } from '@commudle/shared-services';
+import { FooterService } from 'apps/commudle-admin/src/app/services/footer.service';
 
 @Component({
   selector: 'app-user-personal-chat-list',
@@ -8,13 +9,17 @@ import { LayoutService } from '@commudle/shared-services';
   standalone: false,
 })
 export class UserPersonalChatListComponent implements OnInit, OnDestroy {
-  constructor(private layoutService: LayoutService) {}
+  constructor(private layoutService: LayoutService, private footerService: FooterService) {}
 
   ngOnInit(): void {
+    this.layoutService.changeFullHeightContent(false);
     this.layoutService.setShowGlobalChatPopup(false);
+    this.footerService.changeMiniFooterStatus(false);
   }
 
   ngOnDestroy(): void {
+    this.layoutService.changeFullHeightContent(true);
     this.layoutService.setShowGlobalChatPopup(true);
+    this.footerService.changeMiniFooterStatus(true);
   }
 }
